23fc1728d6a1a792d9434539861260e71f0b759e
Eve <hi.its.eve@gmail.com>
2021-11-15 16:42:57 -0500

    remove bass_fx config from tempo attributes

--- src/channel/channel_attributes.rs
+++ src/channel/channel_attributes.rs
@@ -35,34 +35,24 @@ impl Into<u32> for ChannelAttribute {
 
 #[cfg(feature="bass_fx")] 
 pub enum TempoAttribute {
-    #[cfg(feature="bass_fx")] 
     /// The tempo of a channel, [-95%...0...+5000%] percents.
     Tempo,
-    #[cfg(feature="bass_fx")] 
     /// The pitch of a channel, [-60...0...+60] semitones.
     TempoPitch,
-    #[cfg(feature="bass_fx")] 
     /// The sample rate of a channel in Hz, but calculates by the same % as `TEMPO`.
     TempoFrequency,
-    #[cfg(feature="bass_fx")] 
     /// Use the AA Filter for a tempo channel
     TempoUseAAFilter,
-    #[cfg(feature="bass_fx")] 
     /// Sets the AA Filter length in taps (between 8 and 128).
     TempoAAFilterLength,
-    #[cfg(feature="bass_fx")] 
     /// Use the tempo quick algorithm for a tempo channel (true =1, false =0).
     TempoUseQuickAlgorithm,
-    #[cfg(feature="bass_fx")] 
     /// Sets the tempo sequence in ms. of a tempo channel (default = 82).
     TempoSequenceMs,
-    #[cfg(feature="bass_fx")] 
     /// Sets the tempo seek window in ms. of a tempo channel (default = 82).
     TempoSeekWindowMs,
-    #[cfg(feature="bass_fx")] 
     /// Sets the tempo overlap in ms. of a tempo channel (default = 12).
     TempoOverlapMs,
-    #[cfg(feature="bass_fx")] 
     /// Sets the playback direction of a reverse channel (-1=reverse, 1=forward, or use one of the BASSFXReverse flags).
     ReverseDir,
 }
