dfc14083829f32f341aeefaf73d828d34a6fb848
Eve <hi.its.eve@gmail.com>
2021-11-15 21:06:46 -0500

    use proper value for frequency, and wrap init fn in check err

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,11 @@ pub mod bass_flags;
 
 
 pub fn init_default() -> BassResult<()> {
-    bass_sys::BASS_Init(-1, bass_sys::BASS_DEVICE_STEREO, 0, 0 as *mut std::ffi::c_void, 0 as *mut std::ffi::c_void);
+    check_bass_err!(bass_sys::BASS_Init(-1, 44100, bass_sys::BASS_DEVICE_STEREO, 0 as *mut std::ffi::c_void, 0 as *mut std::ffi::c_void));
     Ok(())
 }
 
 pub fn init_default_with_ptr<P>(window_ptr:*mut P) -> BassResult<()> {
-    bass_sys::BASS_Init(-1, bass_sys::BASS_DEVICE_STEREO, 0, window_ptr as *mut std::ffi::c_void, 0 as *mut std::ffi::c_void);
+    check_bass_err!(bass_sys::BASS_Init(-1, 44100, bass_sys::BASS_DEVICE_STEREO, window_ptr as *mut std::ffi::c_void, 0 as *mut std::ffi::c_void));
     Ok(())
 }
